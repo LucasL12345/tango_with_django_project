794cf6f1f4147a3bbca8c7c7f40e164feca28c48
Lucas Lemoine <2457626l@student.gla.ac.uk>
2021-02-05 19:47:37 +0000

    Chapter 5

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -15,9 +15,11 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_DIRS = [STATIC_DIR, ]
-MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
